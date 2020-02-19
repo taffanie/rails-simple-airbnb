985c99d0022a708430f0b59c80e239458b9a6d52
Taffanie Ben <taffanie.ben@gmail.com>
2020-02-19 15:38:10 +0000

    Rails new with frontend and form gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Flat.destroy_all
+
+5.times do
+
+  Flat.create!(
+  name: Faker::Address.community,
+  address: Faker::Address.street_address,
+  description: Faker::TvShows::TheITCrowd.quote,
+  price_per_night: Faker::Number.number(digits: 3),
+  number_of_guests: Faker::Number.number(digits: 1)
+)
+
+end
