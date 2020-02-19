5ba4cf741c06b95fb32351a6ec5167cc0e52efb3
Taffanie Ben <taffanie.ben@gmail.com>
2020-02-19 16:53:02 +0000

    simple airbnb

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -8,4 +8,35 @@ class FlatsController < ApplicationController
     @flat = Flat.find(params[:id])
   end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.create(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
+  end
+
 end
